e70640bd1d9bd618b3df2a74a49d503f2aa636c1
cody koeninger <cody@koeninger.org>
2010-11-20 22:50:12 -0600

    experiment with combinator-based syntax, instead of implicitly added methods

--- src/main/scala/XmlHelpers.scala
+++ src/main/scala/XmlHelpers.scala
@@ -43,4 +43,20 @@ object XmlHelpers {
 
   }
   implicit def richNodeSeq(xs: NodeSeq): RichNodeSeq = new RichNodeSeq(xs)
+
+  def find(selector: String) =
+    (xs: NodeSeq) => (new RichNodeSeq(xs)).find(selector)
+
+  def edit(selector: String, replace: NodeSeq) =
+    (xs: NodeSeq) => (new RichNodeSeq(xs)).edit(selector, replace)
+
+  def chain(fns: NodeSeq => NodeSeq*) =
+    Function.chain(fns)
+
+  implicit def pairToFn(p:(String, NodeSeq)): NodeSeq => NodeSeq =
+    edit(p._1, p._2)
+
+  implicit def pairToFn2(p:(String, NodeSeq => NodeSeq)): NodeSeq => NodeSeq =
+    (xs: NodeSeq) => (new RichNodeSeq(xs)).edit(p._1)(p._2)
+  
 }
--- src/test/scala/XmlHelpersSpec.scala
+++ src/test/scala/XmlHelpersSpec.scala
@@ -1,7 +1,7 @@
 package org.koeninger
 
 import scala.xml._
-import XmlHelpers._
+
 import org.specs._
 
 class XmlHelpersSpec extends Specification {
@@ -18,28 +18,81 @@ class XmlHelpersSpec extends Specification {
       </body>
     </html>
 
-  "find stuff" in {
-    val target = <weird:div id="zoo" class="doo too">different nested div</weird:div>
-    x.find("#zoo").head must_== target
-    x.find(".doo weird|div#zoo.too").head must_== target
-    x.find("div.doo.too").head must_== target
-  }
+  
+  "implicitly added nodeseq methods" should {
+    import XmlHelpers._
+    "find stuff" in {
+      val target = <weird:div id="zoo" class="doo too">different nested div</weird:div>
+      x.find("#zoo").head must_== target
+      x.find(".doo weird|div#zoo.too").head must_== target
+      x.find("div.doo.too").head must_== target
+    }
 
-  "edit stuff with literals" in {
-    x.edit("title.too", <title>A new title</title>).toString mustMatch "<title>A new title</title>"
-  }
+    "edit stuff with literals" in {
+      x.edit("title.too", <title>A new title</title>).toString mustMatch "<title>A new title</title>"
+    }
 
-  "edit stuff with functions" in { 
-    val spans = List("mopey", "dopey", "gropey")
+    "edit stuff with functions" in { 
+      val spans = List("mopey", "dopey", "gropey")
 
-    val one = x.edit(".doo"){ y =>
-      y.edit("weird|div#zoo.too"){ z =>
-        spans.map{ s => <span>{ Text(s) }</span> }}}
+      val one = x.edit(".doo"){ y =>
+        y.edit("weird|div#zoo.too"){ z =>
+          spans.map{ s => <span>{ Text(s) }</span> }}}
 
-    val two = x.edit(".doo weird|div#zoo.too"){ y =>
+      val two = x.edit(".doo weird|div#zoo.too"){ y =>
         spans.map{ s => <span>{ Text(s) }</span> }}
 
-    one must_== two
-    one must_!= x
+      one must_== two
+      one must_!= x
+    }
+  }
+
+  "combinators" should {
+    // find is defined by specs...
+    import XmlHelpers.{ find => fin, _ }
+
+    "find stuff" in {
+      val target = <weird:div id="zoo" class="doo too">different nested div</weird:div>
+
+      fin("#zoo")(x).head must_== target
+      fin(".doo weird|div#zoo.too")(x).head must_== target
+      fin("div.doo.too")(x).head must_== target
+    }
+
+    "edit stuff with literals" in {
+      edit("title.too", <title>A new title</title>)(x).toString mustMatch "<title>A new title</title>"
+    }
+
+    "chain together edits and finds" in {
+      val target: NodeSeq =        
+        <div class="doo">
+          a div <p>noo</p> in it
+        </div>
+        <div class="doo">
+          different div <p>too</p> in it
+        </div>;
+
+      chain(
+        fin("div[class=doo]"),
+        edit(".noo", <p>noo</p>),
+        edit(".too", <p>too</p>)
+      )(x) must_== target
+
+      chain(
+        fin("div[class=doo]"),
+        ".noo" -> <p>noo</p>,
+        ".too" -> <p>too</p>
+      )(x) must_== target
+
+      def pWrap(s: NodeSeq): NodeSeq =
+        <p>{ s.head.attributes("class").head.toString.split(" ")(1) }</p>
+
+      chain(
+        fin("div[class=doo]"),
+        ".noo" -> pWrap _,
+        ".too" -> pWrap _
+      )(x) must_== target
+
+    }
   }
 }
